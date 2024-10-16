15f0d77aa2051dfc9b979826e9ffcc5a976bd942
Eshamova <eshamova72@yandex.ru>
2024-10-16 15:44:48 +0300

    Добавлен попап для написания отзыва

--- app/js/main.js
+++ app/js/main.js
@@ -31,9 +31,42 @@ if (isMobile.any()){
 
 //= ../../node_modules/jquery/dist/jquery.js
 //= ../../node_modules/locomotive-scroll/dist/locomotive-scroll.js
+//= ../../node_modules/swiper/swiper-bundle.js
+//= ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
 $(function () {
   // Плавный скролл
   let scroll = new LocomotiveScroll();
+
+  // Слайдер отзывов
+  const swiperReviews = new Swiper('.reviews__slider', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    breakpoints: {
+      1025: {
+        slidesPerView: 2,
+      },
+    },
+    navigation: {
+      nextEl: '.reviews__btn-next',
+      prevEl: '.reviews__btn-prev',
+    },
+  });
+
+  // Всплывающее окно для отзыва
+    $('.reviews__btn').fancybox({
+      type: 'inline',
+      btnTpl: {
+      smallBtn:
+        '<button class="popup-close-btn" data-fancybox-close><span class="visually-hidden">Закрыть.</span></button>',
+      },
+      afterLoad: function () {
+        $('.reviews__popup').addClass('animate__animated fadeInDown');
+      },
+      beforeClose: function () {
+        $('.reviews__popup').addClass('animate__animated fadeOutDown');
+      },
+    });
+
   //= ../module/_header/_header.js
 
   //= ../module/_footer/_footer.js
