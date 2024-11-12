fa6b27f31bbdc166aee0fd7b4e81ed7b0744e18d
Eshamova <eshamova72@yandex.ru>
2024-11-12 17:13:48 +0300

    Добавлена страница Отзывов

--- app/js/main.js
+++ app/js/main.js
@@ -56,22 +56,22 @@ $(function () {
   });
 
   // Всплывающее окно
-   $('.btn-popup-open').fancybox({
-     type: 'inline',
-     closeExisting: true,
-     btnTpl: {
-       smallBtn:
-         '<button class="popup-close-btn" data-fancybox-close><span class="visually-hidden">Закрыть.</span></button>',
-     },
-     afterLoad: function () {
-       $('.popup').addClass('animate__animated fadeInDown');
-     },
-     beforeClose: function () {
-       $('.popup').addClass('animate__animated fadeOutDown');
-     },
-     backFocus: false,
-     autoFocus: false,
-   });
+  $('.btn-popup-open').fancybox({
+    type: 'inline',
+    closeExisting: true,
+    btnTpl: {
+      smallBtn:
+        '<button class="popup-close-btn" data-fancybox-close><span class="visually-hidden">Закрыть.</span></button>',
+    },
+    afterLoad: function () {
+      $('.popup').addClass('animate__animated fadeInDown');
+    },
+    beforeClose: function () {
+      $('.popup').addClass('animate__animated fadeOutDown');
+    },
+    backFocus: false,
+    autoFocus: false,
+  });
 
   // Проверка удачной отправки формы отзыва
   $('.reviews-popup__form').on('submit', function (e) {
@@ -84,8 +84,10 @@ $(function () {
         setTimeout(() => {
           $.fancybox.getInstance('close');
           $.fancybox.open({
-            src: '#reviews__thanks',
+            src: '#reviews-thanks',
             type: 'inline',
+            backFocus: false,
+            autoFocus: false,
             btnTpl: {
               smallBtn:
                 '<button class="popup-close-btn" data-fancybox-close><span class="visually-hidden">Закрыть.</span></button>',
@@ -124,9 +126,11 @@ $(function () {
 
   /**** Кнопка сброса фильтров тэгов на странице магазина ****/
   // Проверяем наличие применённых тэгов на странице
+  let countFilterChecked = 0;
   $('.filter-tags__input').each(function () {
     if ($(this).prop('checked')) {
       $('.filter-tags__reset').prop('disabled', false);
+      countFilterChecked += 1;
     } else {
       $('.filter-tags__reset').prop('disabled', true);
     }
@@ -136,17 +140,14 @@ $(function () {
   $('.filter-tags__input').on('change', function () {
     if ($(this).prop('checked')) {
       $('.filter-tags__reset').prop('disabled', false);
+      countFilterChecked += 1;
     } else {
-      let numChecked = 0;
-      $('.filter-tags__input').each(function () {
-        if ($(this).prop('checked')) {
-          numChecked += 1;
-        }
-      });
-      if (numChecked == 0) {
+      countFilterChecked -= 1;
+      if (countFilterChecked == 0) {
         $('.filter-tags__reset').prop('disabled', true);
       }
     }
+    console.log(countFilterChecked);
   });
 
   // Обработка события нажатия на кнопку сброса
@@ -158,6 +159,55 @@ $(function () {
   });
 
   /**** /Кнопка сброса фильтров тэгов на странице магазина ****/
+
+  /****  Слайдер отзывов на странице отзывов ****/
+  // Слайдер отзывов
+  const swiperReviewsSurvey = new Swiper('.reviews-survey__slider', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    breakpoints: {
+      1025: {
+        slidesPerView: 3,
+      },
+      701: {
+        slidesPerView: 2,
+      },
+    },
+    navigation: {
+      nextEl: '.slider__btn-next',
+      prevEl: '.slider__btn-prev',
+    },
+  });
+
+  /* Обрезка текста отзывов*/
+  $('.reviews-survey__blockquote').each(function () {
+    let countParagraf = $(this).children('p').length; // Количество параграфов в тексте
+    let sizeReview = $(this).data('trim'); // Длина текста в символах
+    let countSize = sizeReview;
+    $(this)
+      .children('p')
+      .each(function () {
+        // Идём по всем параграфам
+        let contentText = $(this).text();
+        let text = contentText.trim(); // Обрезаем пробелы в начале и в конце
+        if (text.length > countSize) {
+          if (countSize > 0) {
+            text = text.slice(0, countSize);
+            let lastSpace = text.lastIndexOf(' ');
+            if (lastSpace > 0) {
+              // Урезаем до границы целого слова
+              text = text.substring(0, lastSpace);
+              $(this).text(text + '...');
+            }
+            countSize = 0;
+          } else {
+            $(this).text('');
+          }
+        } else {
+          countSize = countSize - text.length;
+        }
+      });
+  });
 
   //= ../module/_footer/_footer.js
 });
